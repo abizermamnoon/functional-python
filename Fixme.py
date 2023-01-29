ce0469f6ce2a1059801247c086165a74a952ed6c
Abizer Mamnoon <amamnoon25@cmc.edu>
2023-01-28 16:17:55 -0800

    used lambda, map and filter

--- Fixme.py
+++ Fixme.py
@@ -20,12 +20,12 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    LC = [x for x in range(0,n + 1) if x%2 == 0]
-    return LC
-print("even(10)=",evens(10))
-print("even(11)=",evens(11))
-print("even(0)=",evens(0))
-print("even(1)=",evens(1))
+    foo = lambda x: x%2 == 0
+    xs = range(n + 1)
+    xs = filter(foo,xs)
+    xs = list(xs)
+    print(xs)
+
 
 def threes(n):
     '''
@@ -42,19 +42,10 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    LC = []
-    for x in range(0, n + 1):
-        x = str(x)
-        for int in x:
-            if int == '3':
-                LC += [x] 
-    LCI = [eval(x) for x in LC]
-    return LCI
-        
-print("threes(2)=",threes(2))
-print("threes(10)=",threes(10))
-print("threes(20)=",threes(20))
-print("threes(50)=",threes(50))
+    xs = filter(lambda x: '3' in str(x), range(0,n + 1))
+    xs = list(xs)
+    print(xs)
+
 
 def small_words(text):
     '''
@@ -73,14 +64,10 @@ def small_words(text):
     ['a', 'big', 'word', 'is', 'bad']
     '''
     text_split = text.split()
-    LC = [x for x in text_split if len(x)<5]
-    return LC
+    xs = filter(lambda x: len(x) < 5, text_split)
+    xs = list(xs)
+    print(xs)
 
-    
-
-print("small_words('this is a simple test case')=", small_words('this is a simple test case'))
-print("small_words('really enormous words')=", small_words('really enormous words'))
-print(" small_words('')=",small_words(''))
 
 def squares(n):
     '''
@@ -96,13 +83,10 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    LC = [x*x for x in range(1,n + 1)]
-    return LC
+    xs = map(lambda x: x*x, range(1, n + 1))
+    xs = list(xs)
+    print(xs)
 
-print("squares(1)=", squares(1))
-print("squares(2)=", squares(2))
-print("squares(3)=", squares(3))
-print("squares(10)=", squares(10))
 
 def lengths(strings):
     '''
@@ -115,9 +99,6 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
-    LC = [len(x) for x in strings]
-    return LC
-
-print("lengths([])=", lengths([]))
-print("lengths(['test'])=", lengths(['test']))
-print("lengths(['this','is','a','test'])=", lengths(['this','is','a','test']))
+    xs = map(lambda x: len(x),strings)
+    xs = list(xs)
+    print(xs)
